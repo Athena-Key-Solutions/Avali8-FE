4ad4507847892935b3a5400c1015e5d37d060b23
Victor Vidal <victor.vidal@ufrpe.br>
2019-11-23 20:02:29 -0300

    Navegação e União com o Back implementadas

--- perfil/header.js
+++ perfil/header.js
@@ -9,3 +9,31 @@ function displayDropDownCont(id)
      document.getElementById(id).style.display = "";
   }
 }
+
+function logout(){  
+   var user = JSON.parse(sessionStorage.getItem('user'));
+   $.ajax({
+      "url": "http://localhost:3333/avali8/api/v1/logout",
+      "type": "POST",
+      "contentType": "application/json",
+      "data": JSON.stringify({"token":user.token}),
+      "success": function(response){
+         console.log(response);
+         sessionStorage.clear();
+         alert("Log out bem sucedido");
+         window.location.replace("../login/login.html");
+      }
+  }); 
+}
+
+function perfil(){
+   window.location.replace("../perfil/perfil.html");
+}
+
+function inicial(){
+   window.location.replace("../tela-inicial-logado/tela-inicial-logado.html");
+}
+
+function editarPerfil(){
+   // window.location.replace("link");
+}
--- perfil/perfil.js
+++ perfil/perfil.js
@@ -11,8 +11,18 @@ function displayEditarPerfil(id){
 
 function editPost(nova)
 {
-  var user = JSON.parse(localStorage.getItem('user'));
-  $.post("/user/edit/:"+user.id, {bio: nova, profile_image: user.profile_image, password: user.password}, function(status){ alert("Atualização da Bio" + "\nStatus:" + status); } );
+  var user = JSON.parse(sessionStorage.getItem('user'));
+  user.bio = text;
+  $.ajax({
+    "url": "http://localhost:3333/avali8/api/v1/user/edit/"+user.id,
+    "type": "POST",
+    "contentType": "application/json",
+    "data": JSON.stringify(user),
+    "success": function(response){
+      alert("Bio alterada com sucesso");
+      windows.reload(true);
+    }
+  });
 }
 
 function attBio()
@@ -59,8 +69,8 @@ function preencherMedalhas(medalhas)
 
 
 $(document).ready(function(){
-  var user = {"id": 1,"name": "Henrique","username": "test343","progress": 0,"level": 1,"profile_image": null,"email": "test53@gmail.com","created_at": "2019-11-02 00:55:45","updated_at": "2019-11-02 00:55:45","bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat velit lorem, quis tristique mi mollis vitae. Cras consequat id ante vitae vehicula. Cras at elit tincidunt, auctor dolor eu, facilisis eros. Curabitur non consectetur tellus. Sed leo arcu, rhoncus nec lobortis vel, convallis et elit.","medalhas": [{certificado: "Python", score:3}, {certificado: "C", score:3}, {certificado: "C#", score:3},{certificado: "C++", score:3},{certificado: "Java", score:3},{certificado: "Ruby", score:2},{certificado: "JavaScript", score:3},{certificado: "Haskell", score:1},{certificado: "Closure", score:1},{certificado: "Ada", score:2},{certificado: "Go", score:3}, {certificado: "Pascal", score:1},{certificado: "Fortran", score:2},{certificado: "Lisp", score:3},{certificado: "Prolog", score:2},{certificado: "Gödel", score:1}]}
-    //var user = JSON.parse(localStorage.getItem('user'));
+  //var user = {"id": 1,"name": "Henrique","username": "test343","progress": 0,"level": 1,"profile_image": null,"email": "test53@gmail.com","created_at": "2019-11-02 00:55:45","updated_at": "2019-11-02 00:55:45","bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat velit lorem, quis tristique mi mollis vitae. Cras consequat id ante vitae vehicula. Cras at elit tincidunt, auctor dolor eu, facilisis eros. Curabitur non consectetur tellus. Sed leo arcu, rhoncus nec lobortis vel, convallis et elit.","medalhas": [{certificado: "Python", score:3}, {certificado: "C", score:3}, {certificado: "C#", score:3},{certificado: "C++", score:3},{certificado: "Java", score:3},{certificado: "Ruby", score:2},{certificado: "JavaScript", score:3},{certificado: "Haskell", score:1},{certificado: "Closure", score:1},{certificado: "Ada", score:2},{certificado: "Go", score:3}, {certificado: "Pascal", score:1},{certificado: "Fortran", score:2},{certificado: "Lisp", score:3},{certificado: "Prolog", score:2},{certificado: "Gödel", score:1}]}
+    var user = JSON.parse(sessionStorage.getItem('user'));
     document.getElementById('username1').innerHTML = user.name;
     document.getElementById('username2').innerHTML = user.name;
     document.getElementById('bio').innerHTML = user.bio;
