8d47dab553c36dbb5aa2f5d02245f677bbda055f
Victor Vidal <victor.vidal@ufrpe.br>
2019-11-27 10:58:17 -0300

    Implementação do Ranking

--- Release/Avali8-FE/tela-inicial-logado/tela-inicial-logado.js
+++ Release/Avali8-FE/tela-inicial-logado/tela-inicial-logado.js
@@ -11,29 +11,40 @@ $(document).ready(function(){
         document.getElementById('username1').innerHTML = response.name;
         sessionStorage.setItem("user", JSON.stringify(response) );
       }
-  });
+  }); 
+  //List users  
+      $.ajax({
+        "url": "http://127.0.0.1:3333/avali8/api/v1/userlist",
+        "type": "POST",
+        "contentType": "application/json",
+        "data": JSON.stringify({"token":user.token}),
+        "success": function(response){
+          preencherGrafico(response);
+        }
+    });
 
-    //Gráfico
-    google.charts.load('current', {'packages':['bar']});
+});
+
+function preencherGrafico(users){
+  
+  var usersScores = [["User\n","Score"]];
+
+  for(x in users){
+    usersScores.push([users[x].name,users[x].progress]);
+  }
+
+  google.charts.load('current', {'packages':['bar']});
       google.charts.setOnLoadCallback(drawStuff);
-
       function drawStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['User', 'Score'],
-          ["User 1", 44],
-          ["User 2", 31],
-          ["User 3", 12],
-          ["User 4", 10],
-          ["User 4", 3]
-        ]);
+        var data = new google.visualization.arrayToDataTable(usersScores);
 
         var options = {
           legend: { position: 'none' },
-          bar: { groupWidth: "90%" }
+          bar: { groupWidth: "90%"}
         };
 
         var chart = new google.charts.Bar(document.getElementById('ranking'));
         // Convert the Classic options to Material options.
         chart.draw(data, google.charts.Bar.convertOptions(options));
       };
-});
+}
